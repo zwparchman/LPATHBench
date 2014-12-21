ddac4adb3f41d9ccbd426429eaf88fed9612803a
zwparchman <zwparchman@gmail.com>
2014-12-21 11:13:49 -0600

    Added caching for small instances of the problem.
    Reduced run time to 60 ms on my machine.

--- cpp.cpp
+++ cpp.cpp
@@ -27,43 +27,87 @@ vector<node> readPlaces(){
   }
   return nodes;
 }
+#include <unordered_map>
+
 
 template <int T>
-int getLongestPath(const vector<node> &nodes, const int nodeID, bitset<T> visited){
-  visited[nodeID] = true;
-  int max=0;
-  for(const route &neighbour: nodes[nodeID].neighbours){
-    if (visited[neighbour.dest] == false){
-      const int dist = neighbour.cost + getLongestPath<T>(nodes, neighbour.dest, visited);
-      if (dist > max){
-        max = dist;
+struct getLongestPath_class{
+
+  int nocache(const vector<node> &nodes, const int nodeID, bitset<T> visited){
+    visited[nodeID] = true;
+    int max=0;
+    for(const route &neighbour: nodes[nodeID].neighbours){
+      if (visited[neighbour.dest] == false){
+        const int dist = neighbour.cost + nocache(nodes, neighbour.dest, visited);
+        if (dist > max){
+          max = dist;
+        }
       }
     }
+    return max;
   }
-  visited[nodeID] = false;
-  return max;
-}
 
-int getLongestPath(const vector<node> &nodes)
-{
+  unordered_map<  bitset<sizeof(const int) + T  > , const int> cmap;
+  int cache (const vector<node> &nodes, const int nodeID, bitset<T> visited){
+
+    long int set = visited.to_ulong();
+    set = set << sizeof( const int );
+    set += nodeID;
+    const bitset<T + sizeof( const int ) > key ( set );
+
+    {
+      auto where = cmap.find( key );
+      if( where != cmap.end() ){
+        return where->second;
+      }
+    }
+
+    visited[nodeID] = true;
+    int max=0;
+    for(const route &neighbour: nodes[nodeID].neighbours){
+      if (visited[neighbour.dest] == false){
+        const int dist = neighbour.cost + cache(nodes, neighbour.dest, visited);
+        if (dist > max){
+          max = dist;
+        }
+      }
+    }
+
+    cmap.emplace( key , max );
+    return max;
+  }
+
+};  
+
+
+int getLongestPath(const vector<node> &nodes) {
   if (nodes.size() <= 16) {
-     return getLongestPath<16>(nodes, 0, bitset<16>());
+    static getLongestPath_class<16> c;
+    return c.cache(nodes, 0, bitset<16>());
+  }  else if ( nodes.size() <= 32 ){
+    static getLongestPath_class<32> c;
+    return c.cache(nodes, 0, bitset<32>());
   } else if (nodes.size() <= 256) {
-    return getLongestPath<256>(nodes, 0, bitset<256>());
+    static getLongestPath_class<256> c;
+    return c.cache(nodes, 0, bitset<256>());
   } else if (nodes.size() <= 4096) {
-    return getLongestPath<4096>(nodes, 0, bitset<4096>());
+    static getLongestPath_class<4096> c;
+    return c.nocache(nodes, 0, bitset<4096>());
   } else if (nodes.size() <= 65536) {
-    return getLongestPath<65536>(nodes, 0, bitset<65536>());
+    static getLongestPath_class<65536> c;
+    return c.nocache(nodes, 0, bitset<65536>());
   } else if (nodes.size() <= 1048576) {
-    return getLongestPath<1048576>(nodes, 0, bitset<1048576>());
+    static getLongestPath_class<1048576> c;
+    return c.nocache(nodes, 0, bitset<1048576>());
   } else if (nodes.size() <= 16777216) {
-    return getLongestPath<16777216>(nodes, 0, bitset<16777216>());
+    static getLongestPath_class<16777216> c;
+    return c.nocache(nodes, 0, bitset<16777216>());
   } else {
     return -1;
   }
 }
 
-int main(int argc, char** argv){
+int main(int , char** ){
   auto nodes = readPlaces();
   auto start = high_resolution_clock::now();
   int len = getLongestPath(nodes);
